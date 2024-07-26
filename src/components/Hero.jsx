109067568690f9f136e2dd5e2680abf19856feb2
MarlonBair <marlonbair1@gmail.com>
2024-07-25 20:58:07 -0700

    Boilerplate

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return(
+        <div className="hero">
+            <h1 className="hero-title">Coming Soon</h1>
+            <img className="hero-image" src="./logo.png" alt="SLPN logo"/>
+        </div>
+    );
+}
+
+export default Hero
